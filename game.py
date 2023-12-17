0e3b759f734618c14913b8e90541042b28450f97
Chris <81205126+TalentedB@users.noreply.github.com>
2023-12-17 06:51:31 -0800

    Idk

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ RED = (255, 0, 0)
 class Sprite(pygame.sprite.Sprite):  
     def __init__(self, pos):  
         pygame.sprite.Sprite.__init__(self)  
-        self.image = pygame.Surface([20, 20])  
+        self.image = pygame.Surface([blockSize, blockSize])  
         self.image.fill(GREEN)  
         self.rect = self.image.get_rect()  
         self.rect.center = pos  
@@ -19,7 +19,7 @@ class Sprite(pygame.sprite.Sprite):
 class FoodSprite(pygame.sprite.Sprite):
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([20, 20])  
+        self.image = pygame.Surface([blockSize, blockSize])  
         self.image.fill(RED)  
         self.rect = self.image.get_rect()  
         self.rect.center = pos
@@ -28,23 +28,38 @@ class GameState:
     def __init__(self, gridSize):
         self.grid = [[0 for x in range(gridSize)] for y in range(gridSize)]
         self.PlayerSize = 1
+        self.movementDirection = [1, 0]
+        self.PlayerBody = [(0, 0)]
+        self.FoodSpawned = 0
 
 pygame.init()  
 clock = pygame.time.Clock()  
-fps = 50
+fps = 5
 bg = [0, 0, 0]  
 screenSize =[600, 600]  
 screen = pygame.display.set_mode(screenSize)  
-player = Sprite([40, 50])  
-# Define keys for player movement  
-player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
-player.vx = 5  
-player.vy = 10
 
-blockSize = 20
+blockSize = 50
 gridSize = int(screenSize[0] // blockSize)
 gameState = GameState(gridSize)
 food_group = pygame.sprite.Group() 
+
+
+
+player = Sprite([blockSize/2, blockSize/2])  
+
+# Define keys for player movement  
+player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
+player.vx = blockSize
+player.vy = blockSize
+
+
+
+print("Screen size: " + str(screenSize))
+print("Block size: " + str(blockSize))
+
+print("Grid size: " + str(gridSize))
+
 
 def drawGrid():
     for x in range(0, screenSize[0], blockSize):
@@ -52,28 +67,65 @@ def drawGrid():
             rect = pygame.Rect(x, y, blockSize, blockSize)
 
             pygame.draw.rect(screen, getGridPosColor((x//blockSize,y//blockSize)), rect, 1)
-            if random.randint(0, 100) == 1:
-                food = FoodSprite([x, y])  
-                food_group.add(food)  
-            
+
+
+
+def spawnFood():
+    if gameState.FoodSpawned == 0:
+        random_x = round(random.randint(0, screenSize[0] - 1))//blockSize * blockSize
+        random_y = round(random.randint(0, screenSize[1] - 1))//blockSize * blockSize
+        # print("Random x: " + str(random_x) + " Random y: " + str(random_y))
+
+        gameState.grid[random_x//blockSize][random_y//blockSize] = 2
+        gameState.FoodSpawned = 1
+
+        food = FoodSprite([random_x + blockSize/2, random_y + blockSize/2])  
+        food_group.add(food)
+
+
+def movePlayerBody():
+    for i in range(len(gameState.PlayerBody)):
+        if i == 0:
+            gameState.PlayerBody[i] = (player.rect.x, player.rect.y)
+        else:
+            gameState.PlayerBody[i] = gameState.PlayerBody[i-1]
+    if gameState.FoodSpawned == 0:
+        gameState.PlayerBody.append(gameState.PlayerBody[-1])
+        # gameState.PlayerBody.pop()
+    
+    print(gameState.PlayerBody)
+
 
 def getGridPosColor(pos):
     if gameState.grid[pos[0]][pos[1]] == 0:
+        # return [WHITE, RED][random.randint(0, 1)]
         return WHITE
     if gameState.grid[pos[0]][pos[1]] == 1:
         return GREEN
     if gameState.grid[pos[0]][pos[1]] == 2:
         return RED
 
+def movePlayer(key):
+    if key:
+        if key[player.move[0]]:
+            player.rect.x -= player.vx
+            gameState.movementDirection = [-1, 0]
+        elif key[player.move[1]]:
+            player.rect.x += player.vx
+            gameState.movementDirection = [1, 0]
+        elif key[player.move[2]]:
+            player.rect.y -= player.vy
+            gameState.movementDirection = [0, -1]
+        elif key[player.move[3]]:
+            player.rect.y += player.vy
+            gameState.movementDirection = [0, 1]
+    else:
+        player.rect.x += player.vx * gameState.movementDirection[0]
+        player.rect.y += player.vy * gameState.movementDirection[1]
+
+
 def main():  
  
-    # Add random food
-    for i in range(10):
-        random_x = random.randint(0, screenSize[0])
-        random_y = random.randint(0, screenSize[1])
-        food = FoodSprite([random_x, random_y])  
-        food_group.add(food)  
-  
     player_group = pygame.sprite.Group()  
     player_group.add(player)  
   
@@ -81,30 +133,35 @@ def main():
         for event in pygame.event.get():  
             if event.type == pygame.QUIT:  
                 return False  
+        
+        # Movement
         key = pygame.key.get_pressed()  
-        for i in range(2):  
-            if key[player.move[i]]:  
-                player.rect.x += player.vx * [-1, 1][i]  
-  
-        for i in range(2):  
-            if key[player.move[2:4][i]]:  
-                player.rect.y += player.vy * [-1, 1][i]  
+        movePlayer(key)
+        
+        # Draw
         screen.fill(bg)  
         drawGrid()
-        # first parameter takes a single sprite  
-        # second parameter takes sprite groups  
-        # third parameter is a kill command if true  
+        movePlayerBody()
+        spawnFood()
+        
+
+        # Check for collisions
         food_hit = pygame.sprite.spritecollide(player, food_group, True)  
         if food_hit:
             gameState.PlayerSize += 1
-            print("Player size: " + str(gameState.PlayerSize))
+            gameState.FoodSpawned = 0
+            print(food_hit[0].rect.center)
+            # print("Player size: " + str(gameState.PlayerSize))
 
 
         player_group.draw(screen)  
-        food_group.draw(screen)  
+        food_group.draw(screen) 
+
         pygame.display.update()  
         clock.tick(fps)  
     pygame.quit()  
     sys.exit  
+
+
 if __name__ == '__main__':
     main()
