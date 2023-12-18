eefc7ad4978a994dff31ea6064595132d8a2c72d
Chris <81205126+TalentedB@users.noreply.github.com>
2023-12-18 01:04:16 -0800

    Finally a fully working version of snake

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame  
 import sys  
 import random
+import time
 #Sprite class   
 
 BLACK = (0, 0, 0)
@@ -10,7 +11,7 @@ RED = (255, 0, 0)
 
 MAX_FOOD = 1
 PLAYER_SPAWN = [2, 0]
-
+MAP_LOOP = False
 class GameState:
     def __init__(self, gridSize):
         self.grid = [[0 for x in range(gridSize)] for y in range(gridSize)]
@@ -37,37 +38,54 @@ gameState = GameState(gridSize)
 # Define keys for player movement  
 playerMoves = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
 
-
+def printGrid():
+    print("\n"*200)
+    for i in gameState.grid:
+        for j in i:
+            print(j, end=" ")
+        print()
 
 def movePlayerBody():
     for i in range(0, len(gameState.PlayerBody)):
         gameState.grid[gameState.PlayerBody[i][0]][gameState.PlayerBody[i][1]] = 0
 
     if len(gameState.PlayerBody) < gameState.PlayerSize:
-        while len(gameState.PlayerBody) < gameState.PlayerSize:
-            gameState.PlayerBody.append([gameState.PlayerHead[0], gameState.PlayerHead[1]])
+        # gameState.PlayerBody.append([gameState.PlayerHead[0], gameState.PlayerHead[1]])
+        gameState.PlayerBody.insert(0, [gameState.PlayerHead[0], gameState.PlayerHead[1]])
     else:
         gameState.PlayerBody.pop()
         gameState.PlayerBody.insert(0, [gameState.PlayerHead[0], gameState.PlayerHead[1]])
 
 
-    for i in range(len(gameState.PlayerBody)):
-        gameState.grid[gameState.PlayerBody[i][0]][gameState.PlayerBody[i][1]] = 1
+    for part in gameState.PlayerBody:
+        gameState.grid[part[0]][part[1]] = 1
+    gameState.grid[gameState.PlayerHead[0]][gameState.PlayerHead[1]] = 3
 
 
 def checkBoundary():
-    if gameState.PlayerHead[0] > len(gameState.grid) - 1:
-        gameState.PlayerHead[0] = 0
-        gameState.PlayerBody[0] = [0, gameState.PlayerHead[1]]
-    if gameState.PlayerHead[0] < 0:
-        gameState.PlayerHead[0] = len(gameState.grid) - 1
-        gameState.PlayerBody[0] = [len(gameState.grid) - 1, gameState.PlayerHead[1]]
-    if gameState.PlayerHead[1] > len(gameState.grid) - 1:
-        gameState.PlayerHead[1] = 0
-        gameState.PlayerBody[0] = [gameState.PlayerHead[0], 0]
-    if gameState.PlayerHead[1] < 0:
-        gameState.PlayerHead[1] = len(gameState.grid) - 1
-        gameState.PlayerBody[0] = [gameState.PlayerHead[0], len(gameState.grid) - 1]
+    if MAP_LOOP:
+        if gameState.PlayerHead[0] > len(gameState.grid) - 1:
+            gameState.PlayerHead[0] = 0
+            gameState.PlayerBody[0] = [0, gameState.PlayerHead[1]]
+        if gameState.PlayerHead[0] < 0:
+            gameState.PlayerHead[0] = len(gameState.grid) - 1
+            gameState.PlayerBody[0] = [len(gameState.grid) - 1, gameState.PlayerHead[1]]
+        if gameState.PlayerHead[1] > len(gameState.grid) - 1:
+            gameState.PlayerHead[1] = 0
+            gameState.PlayerBody[0] = [gameState.PlayerHead[0], 0]
+        if gameState.PlayerHead[1] < 0:
+            gameState.PlayerHead[1] = len(gameState.grid) - 1
+            gameState.PlayerBody[0] = [gameState.PlayerHead[0], len(gameState.grid) - 1]
+    else:
+        if gameState.PlayerHead[0] > len(gameState.grid) - 1:
+            return True
+        if gameState.PlayerHead[0] < 0:
+            return True
+        if gameState.PlayerHead[1] > len(gameState.grid) - 1:
+            return True
+        if gameState.PlayerHead[1] < 0:
+            return True
+    return False
 
 
 def checkCollision():
@@ -82,11 +100,14 @@ def getGridPosColor(pos):
         # Empty
         return WHITE
     if gameState.grid[pos[0]][pos[1]] == 1:
-        # Player
+        # Player Body
         return GREEN
     if gameState.grid[pos[0]][pos[1]] == 2:
         # Food
         return RED
+    if gameState.grid[pos[0]][pos[1]] == 3:
+        # Player Head
+        return BLACK
 
 
 def movePlayer(key):
@@ -111,7 +132,7 @@ def checkFoodEaten():
     if gameState.grid[gameState.PlayerHead[0]][gameState.PlayerHead[1]] == 2:
         gameState.FoodSpawned -= 1
         gameState.PlayerSize += 1
-        gameState.grid[gameState.PlayerHead[0]][gameState.PlayerHead[1]] = 1
+        gameState.grid[gameState.PlayerHead[0]][gameState.PlayerHead[1]] = 3
 
 def drawGrid():
     for x in range(0, screenSize[0], blockSize):
@@ -146,29 +167,37 @@ def main():
         # Movement
         key = pygame.key.get_pressed()  
         movePlayer(key)
-        checkBoundary()
+
+        if checkBoundary():
+            print("Game Over")
+            break
+
         spawnFood()
         checkFoodEaten()
 
         
 
         movePlayerBody()
-        print(gameState.PlayerBody)
-        # Draw
+
+        # ! Draw
         screen.fill(bg)  
         drawGrid()
         
-        
-        
-        # Check for collisions
-        
-
+        # Check for self collisions
         if checkCollision():
             print("Game Over")
-            # return 0
+            break
 
         pygame.display.update()  
         clock.tick(fps)  
+
+    # ! Game Over
+    font = pygame.font.SysFont('arial', 40)
+    gameOver = font.render("Game Over", True, RED)
+    screen.blit(gameOver, (screenSize[0]/2 - gameOver.get_width()/2, screenSize[1]/2 - gameOver.get_height()/2))
+    pygame.display.update()
+
+    # Close the window and quit.
     pygame.quit()  
     sys.exit  
 
